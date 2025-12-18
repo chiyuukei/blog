9385d5179c3cd398875656a9543b78a3331f0b3b
ゆう <247145247+chiyuukei@users.noreply.github.com>
2025-12-18 12:36:24 +0700

    Update bg.js

--- assets/js/bg.js
+++ assets/js/bg.js
@@ -1,1 +1,16 @@
+const backgroundImages = [
+    '//raw.githubusercontent.com/chiyuukei/blog/main/assets/img/bg.jpg',
+    '//raw.githubusercontent.com/chiyuukei/blog/main/assets/img/bg2.jpg',
+    '//raw.githubusercontent.com/chiyuukei/blog/main/assets/img/bg3.jpg'
+  ];
 
+  let currentIndex = 0;
+  const interval = 1 * 60 * 1000;
+
+  function changeBackground() {
+    document.body.style.backgroundImage = `url(${backgroundImages[currentIndex]})`;
+    currentIndex = (currentIndex + 1) % backgroundImages.length;
+  }
+
+  setInterval(changeBackground, interval);
+  changeBackground();
